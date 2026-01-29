e55bb27aa82b114ae790387612c11a0315d69aea
i3mmmm <i3mm@qq.com>
2026-01-29 10:29:33 +0800

    Update backend-api-server.js

--- backend-api-server.js
+++ backend-api-server.js
@@ -15,7 +15,7 @@ const puppeteer = require('puppeteer');
 
 // 初始化Express应用
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // 配置CORS
 app.use(cors({
